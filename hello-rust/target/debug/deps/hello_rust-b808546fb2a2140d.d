1b1709147ff183df9bf530766a84aa9317ee6022
Zachary Carlson <zachary.jk.carlson@proton.me>
2026-09-13 23:23:56 -0700

    Initial setup and Hello World run

---
+++ hello-rust/target/debug/deps/hello_rust-b808546fb2a2140d.d
@@ -0,0 +1,5 @@
+/home/user/@repo/LEARN_RUST_PROJECTS/workspace/Learn-Rust/hello-rust/target/debug/deps/hello_rust-b808546fb2a2140d.d: src/main.rs
+
+/home/user/@repo/LEARN_RUST_PROJECTS/workspace/Learn-Rust/hello-rust/target/debug/deps/hello_rust-b808546fb2a2140d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/user/@repo/LEARN_RUST_PROJECTS/workspace/Learn-Rust/hello-rust/target/debug/hello-rust: /home/user/@repo/LEARN_RUST_PROJECTS/workspace/Learn-Rust/hello-rust/src/main.rs
